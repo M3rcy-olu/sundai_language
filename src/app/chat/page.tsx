2bf1933c7ad41ce6cf5229805272ae655f1be37d
MideCS <mide@mit.edu>
2025-01-26 18:41:04 -0500

    animationnnn

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -4,14 +4,17 @@ import Navbar from "../components/navbar";
 import ChatBot from "../components/chatbot";
 import Subtext from "../components/subtext";
 import Transcription from "./components/transcription";
+import PageTransition from "../components/PageTransition";
 
 export default function Chat() {
   return (
-    <main className="page-alignment gap-[10vh]">
-      <Navbar />
-      <ChatBot showText={false} />
-      <Transcription />
-      <LLMQuery />
-    </main>
+    <PageTransition>
+      <main className="page-alignment gap-[10vh]">
+        <Navbar />
+        <ChatBot showText={false} />
+        <Transcription />
+        <LLMQuery />
+      </main>
+    </PageTransition>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import Button from "./components/button";
 import Navbar from "./components/navbar";
 import Subtext from "./components/subtext";
 import Header from "./components/header";
+import PageTransition from "./components/PageTransition";
 
 export default function Home() {
   // const router = useRouter();
@@ -14,34 +15,35 @@ export default function Home() {
     <main className="page-alignment gap-[14vh]">
       {/* Navbar */}
       <Navbar />
+      <PageTransition>
+        {/* Main Content */}
+        <div className="frame-2">
+          <div className="frame-1">
+            <h1 className="title">
+              <Header text="Speak " gradient={true} />
+              <Header text="Language. Don't just 'Learn'." />
+            </h1>
 
-      {/* Main Content */}
-      <div className="frame-2">
-        <div className="frame-1">
-          <h1 className="title">
-            <Header text="Speak " gradient={true} />
-            <Header text="Language. Don't just 'Learn'." />
-          </h1>
-
-          <div className="subtext">
-            <p className="w-[25vw]">
-              <Subtext text="With " />{" "}
-              <Subtext text="Tongue" className="gradient" />
-              <Subtext text=", learning a new 'tongue' has never been easier." />
-            </p>
-            <br />
-            <p className="w-[25vw]">
-              <Subtext text="Learn your new " />
-              <Subtext text="Tongue " className="gradient" />
-              <Subtext text="(Spanish) now." />
-            </p>
+            <div className="subtext">
+              <p className="w-[25vw]">
+                <Subtext text="With " />{" "}
+                <Subtext text="Tongue" className="gradient" />
+                <Subtext text=", learning a new 'tongue' has never been easier." />
+              </p>
+              <br />
+              <p className="w-[25vw]">
+                <Subtext text="Learn your new " />
+                <Subtext text="Tongue " className="gradient" />
+                <Subtext text="(Spanish) now." />
+              </p>
+            </div>
           </div>
+          <ChatBot showText={true} />
         </div>
-        <ChatBot />
-      </div>
-      <Link href="/chat" className="order-[3]">
-        <Button text="Speak Now" />
-      </Link>
+        <Link href="/chat" className="order-[3]">
+          <Button text="Speak Now" />
+        </Link>
+      </PageTransition>
     </main>
   );
 }
